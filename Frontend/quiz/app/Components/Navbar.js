7cb5647913c81fdaf75ad1343d19c00c40c35b8b
SuperexMack <unilexerox@gmail.com>
2024-12-08 23:43:04 +0530

    Last i think

--- Frontend/quiz/app/Components/Navbar.js
+++ Frontend/quiz/app/Components/Navbar.js
@@ -6,11 +6,11 @@ export default function Navbar(){
     return(
         <>
           <div className="w-full h-[130px] flex items-center bg-slate-100 ">
-            <Image alt="icon-image" className="h-[120px] w-[350px] ml-[50px]" src={quizImage}></Image>
+            <Image alt="icon-image" className="h-[120px] w-[350px] ml-[50px] rounded-xl" src={quizImage}></Image>
             <ul className="flex space-x-10 text-[30px] font-bold absolute right-[50px] text-purple-700">
                 <li className="hover:relative hover:top-4 hover:cursor-pointer"><Link href="/">Home</Link></li>
                 <li className="hover:relative hover:top-4 hover:cursor-pointer">About</li>
-                <li className="hover:relative hover:top-4 hover:cursor-pointer"><Link href="/setmcqs">Create A Quiz</Link></li>
+                <li className="hover:relative hover:top-4 hover:cursor-pointer"><Link href="/setmcqs">Create A Mock-Test</Link></li>
                 <li className="hover:relative hover:top-4 hover:cursor-pointer"><Link href="/Auth/Signin">Login</Link></li>
                 <li className="hover:relative hover:top-4 hover:cursor-pointer"><Link href="/Auth/Signup">Register</Link></li>
                 <li className="hover:relative hover:top-4 hover:cursor-pointer">Contact</li>
